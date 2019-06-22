d627b9c780a9811aaa2757d12edf3a6413cde8f8
deletejeff <deletejeff@qq.com>
2019-06-22 15:29:40 +0800

    西楚电子数据接口项目

---
+++ src/main/java/com/xichu/data_interface/DataInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.xichu.data_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataInterfaceApplication.class, args);
+    }
+
+}
